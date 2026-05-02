b98bbc30b7d1e9dfab03518a0dc3a7aa03338858
kkemboi <kemboikelvin185@gmail.com>
2026-05-01 20:41:05 -0400

    Parsed client message into method and path

--- src/server.cpp
+++ src/server.cpp
@@ -2,6 +2,7 @@
 #include <sys/socket.h> //for socket funcitons ie, socket, bind, listen..
 #include <netinet/in.h> //network structs- for address
 #include <unistd.h> //close
+#include <string>
 using namespace std;
 
 int main() {
@@ -17,23 +18,36 @@ int main() {
     bind(server, (sockaddr*)&addr, sizeof(addr)); //attach socket by address pointer
     listen(server, 1); //listen 
 
-    std::cout<<"Server listening on port 8080";
+    cout<<"Server listening on port 8080"<<endl;
 
     int client=accept(server, nullptr, nullptr); //accept client- socket for client conneection
 
     //read request
     char buffer[4096]={}; //make a char array with size 4kb
     recv(client, buffer, sizeof(buffer),0); //read client message and store in buffer
-    cout<<buffer<<"\n"; //print message stored
+    cout<<"Message from client: "<<buffer<<endl; //print message stored
+
+    string request(buffer); //turns into string
     
-    //response
-    const char* msg =
+    string firstline= request.substr(0, request.find("\r\n")); //firstline
+    size_t space=firstline.find(' '); //first space before path
+    size_t space2=firstline.find(' ', space+1); //second space after path
+
+    string method=firstline.substr(0, space); //everything till first space
+    string path=firstline.substr(space+1, space2-space-1); //path
+
+    cout<<"Method: "<<method<<endl;
+    cout<<"Path: "<<path<<endl;
+
+    //Hardcoded HTTP response
+    string body="What's up! This is my simple practice server";
+    string response =
         "HTTP/1.1 200 OK\r\n"
         "Content-Type: text/plain\r\n"
-        "\r\n"
-        "What's up! This is my simple practice server";
+        "Content-Length: "+ to_string(body.size()) + "\r\n"
+        "\r\n" + body;
 
-    send(client, msg, str_len(msg), 0); //send response to client
+    send(client, response.c_str(), response.size(), 0); //convert repsonse into s-string and send to client
 
     //close both server and client
     close(client);
